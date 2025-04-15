4ab62465b73b95618c6b71c13aa5987016ab4a16
Ederson Moraes <eder.israel.moraes@gmail.com>
2025-04-15 14:28:48 +0100

    fix deploy vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,6 +36,7 @@ mongoose
 );
 
 app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
@@ -43,6 +44,6 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 app.use(require('./routes'));
 
 app.listen(process.env.PORT || 3333, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log('Server is running');
 }
 );
